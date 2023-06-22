8240c5b2afe910d1fdb9398afac47f6b31d40fa5
cega9 <137433334+cega9@users.noreply.github.com>
2023-06-22 11:10:47 -0600

    Delete src directory
    Delete to add correct directories with correct files

--- src/main/java/com/example/base/repository/UsuarioRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.base.repository;
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.base.models.UsuarioModel;
-
-public interface UsuarioRepository extends CrudRepository<UsuarioModel, Long>{
-    
-}
